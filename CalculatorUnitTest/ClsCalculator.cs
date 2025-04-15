3ee5357062aae8c030e9b4cac0e95173afe064f0
Egemen Enis Ertaş <84728316+egemenenis@users.noreply.github.com>
2025-04-15 10:42:43 +0300

    Initial create

---
+++ CalculatorUnitTest/ClsCalculator.cs
@@ -0,0 +1,25 @@
+﻿namespace CalculatorUnitTest
+{
+    public class ClsCalculator
+    {
+        public int addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int division(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ TestCalculator/CalculateTest.cs
@@ -0,0 +1,42 @@
+﻿namespace TestCalculator
+{
+    [TestClass]
+    public sealed class CalculateTest
+    {
+        [TestMethod]
+        public void Addition()
+        {
+            CalculatorUnitTest.ClsCalculator _calculator = new CalculatorUnitTest.ClsCalculator();
+            int result = _calculator.addition(20, 10);
+
+            Assert.AreEqual(30, result);
+        }
+
+        [TestMethod]
+        public void Subtraction()
+        {
+            CalculatorUnitTest.ClsCalculator _calculator = new CalculatorUnitTest.ClsCalculator();
+            int result = _calculator.subtraction(50, 10);
+
+            Assert.AreEqual(40, result);
+        }
+
+        [TestMethod]
+        public void Multiplication()
+        {
+            CalculatorUnitTest.ClsCalculator _calculator = new CalculatorUnitTest.ClsCalculator();
+            int result = _calculator.multiplication(8, 3);
+
+            Assert.AreEqual(24, result);
+        }
+
+        [TestMethod]
+        public void Division()
+        {
+            CalculatorUnitTest.ClsCalculator _calculator = new CalculatorUnitTest.ClsCalculator();
+            int result = _calculator.division(70, 35);
+
+            Assert.AreEqual(2, result);
+        }
+    }
+}
